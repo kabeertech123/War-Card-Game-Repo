66e38560c43eccf93b753c8f0e19e1ee656eed0f
Kabeer Warsi <kwarsi541@gmail.com>
2026-05-03 20:45:41 +0100

    Learned about the Spacer() and how its dynamic, so rather then defining where something goes which will lead to changes on different devices Using Spacer() will fix that issue

--- War Card Game/War Card Game/ContentView.swift
+++ War Card Game/War Card Game/ContentView.swift
@@ -9,16 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Image("background-cloth")
+            
+            
+            
+            VStack () { // logo
+                
+                Spacer()
+                Image("logo")
+                Spacer()
+                //.border(.black, width: 2)
+                
+                
+                
+                HStack (){
+                    // cards
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    
+                }
+                
+                Spacer()
+                
+                VStack () {
+                    //buttons
+                    
+                    Image("button")
+                }
+                Spacer()
+                
+                
+                HStack(){
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline).padding()
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline).padding()
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    
+                    
+                }.foregroundStyle(.white)
+                   
+                
+                Spacer()
+            }
+          
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
+
