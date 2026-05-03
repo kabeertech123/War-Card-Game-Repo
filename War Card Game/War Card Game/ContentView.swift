9b0f9e617af47dcb20e17449df842f9894131879
Kabeer Warsi <kwarsi541@gmail.com>
2026-05-03 20:24:32 +0100

    Added basic design by watching a tutorial No functionality yet
    :)

---
+++ War Card Game/War Card Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Kabeer Warsi on 03/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Kabeer Warsi on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
